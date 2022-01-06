02ce6227bbb23801b657cac35ccb1b2c1991c1fc
truginis <tadas.ruginis@brixmor.com>
2022-01-06 16:40:15 -0500

    added console logs and some comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,11 +15,13 @@ export class AppComponent {
   }
 
   processData(start = 0, pageLimit = 5) {
-    this.appService.processPaginatedAPI(0)
+    console.log(`Processing page ${this.currentPage} of ${pageLimit}`)
+    this.appService.processPaginatedAPI(this.currentPage)
       .subscribe(data => {
           // I am pushing data to an array to just show it in the UI
           // you can do whatever data processing or transformation here.
           this.passengers.push(...data);
+          console.log('Got some data. Transforming...')
 
           // this would be "start" point for getting data
           // you would keep going until there are fewer records than the limit,
@@ -28,7 +30,10 @@ export class AppComponent {
 
           // if limit is not reached go to next page/next batch
           if (this.currentPage < pageLimit) {
+            console.log(`Page limit has not been reached. Let's get some more data`)
             this.processData(this.currentPage);
+          } else {
+            console.log(`Reached the end of the limit. We're done here.`)
           }
       })
   }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -7,17 +7,18 @@ import {filter, map, Observable} from "rxjs";
 })
 export class AppService {
 
-  passengers = [];
-
   constructor(private http: HttpClient) {
   }
 
   processPaginatedAPI(page: number): Observable<any> {
     const size = 10;
+    console.log('Hitting up an API')
     return this.http.get(`https://api.instantwebtools.net/v1/passenger?page=${page}&size=${size}`)
       .pipe(
         map((res: any) => {
+          // just getting the data I care about. which is just an array of passengers.
           const data = res.data;
+          // filtering out any passengers that do not have a name assigned.
           return data.filter( (passenger: any) => passenger.name !== null)
         }),
       )
